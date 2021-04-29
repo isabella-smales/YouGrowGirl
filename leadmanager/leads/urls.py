126811202fb492aea5c6889a1d624c2111365acf
Saif Mustafa <68763259+SMGLOBAL-ops@users.noreply.github.com>
2021-04-29 22:24:41 +0100

    React frontend connected to CRUD Django Rest API

--- leadmanager/leads/urls.py
+++ leadmanager/leads/urls.py
@@ -1,9 +1,15 @@
 from rest_framework import routers
 from .api import LeadViewSet
+from django.views.generic import TemplateView
+from django.urls import path, include
 
 router = routers.DefaultRouter()
 # register routes
 router.register('api/leads', LeadViewSet, 'leads')
 
 # give urls registered for this endpoint
-urlpatterns = router.urls
+urlpatterns = [
+    path('', TemplateView.as_view(template_name='index.html')), 
+    path('/api', include(router.urls)), 
+]
+
